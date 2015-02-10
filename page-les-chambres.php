a21b33602c5a318a1180f5dff7164e66c1ff66e5
oeilgauche <ludovic@oeilgauche.com>
2015-02-10 22:19:17 +0100

    Page chambres

--- page-les-chambres.php
+++ page-les-chambres.php
@@ -1,7 +1,4 @@
 <?php
-/*
-Template Name: Full Width
-*/
 get_header(); ?>
 <div class="row">
 	<div class="small-12 large-12 columns" role="main">
@@ -19,10 +16,30 @@ get_header(); ?>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
-			<?php // comments_template(); ?>
 		</article>
 	<?php endwhile; // End the loop ?>
-
+<div class="row">
+	<ul class="large-block-grid-2 feature">
+<?php 
+// Liens vers les 3 espaces
+//$velo_query = new WP_Query('pagename=au-velocipede');
+$velo_query = new WP_Query(array(
+			'post_type' => 'page',
+			'post__in' => array( 20, 22) ));
+while ( $velo_query->have_posts() ) {
+	$velo_query->the_post();
+?>	
+		<li>
+			<?php the_post_thumbnail(array( 'class' => 'th' )); ?>
+			<h4><?php echo get_the_title(); ?></h1>
+			<p><?php echo get_post_meta( get_the_ID(), 'space_description', true ); ?></p>
+		</li>
+<?php
+}
+wp_reset_postdata();
+?>
+	</ul>
+</div>
 	</div>
 </div>
 
