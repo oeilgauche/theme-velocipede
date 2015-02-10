d7ff1c82a517834e526ea59063b29c973a4e34fd
oeilgauche <ludovic@oeilgauche.com>
2015-02-10 22:09:27 +0100

    Footer et page chambres

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 </section>
 
-<footer class="row">
-	<?php do_action('foundationPress_before_footer'); ?>
-	<?php dynamic_sidebar("footer-widgets"); ?>
-	<?php do_action('foundationPress_after_footer'); ?>
+<footer>
+	<section class="row">
+		<?php do_action('foundationPress_before_footer'); ?>
+		<?php dynamic_sidebar("footer-widgets"); ?>
+		<?php do_action('foundationPress_after_footer'); ?>
+	</section>
 </footer>
 <a class="exit-off-canvas"></a>
 
