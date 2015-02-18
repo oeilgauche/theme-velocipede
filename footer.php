305e194100e6a6c8fa28b9dc151e4cf578f83f9b
oeilgauche <ludovic@oeilgauche.com>
2015-02-18 16:50:43 +0100

    Footer et design

--- footer.php
+++ footer.php
@@ -3,7 +3,51 @@
 <footer>
 	<section class="row">
 		<?php do_action('foundationPress_before_footer'); ?>
-		<?php dynamic_sidebar("footer-widgets"); ?>
+		<?php //dynamic_sidebar("footer-widgets"); ?>
+		<?php
+			$principal = array(
+				'theme_location'  => '',
+				'menu'            => 'principal',
+				'container'       => '',
+				'container_class' => '',
+				'container_id'    => '',
+				'menu_class'      => 'inline-list',
+				'menu_id'         => 'footer-menu-principal',
+				'echo'            => true,
+				'fallback_cb'     => 'wp_page_menu',
+				'before'          => '',
+				'after'           => '',
+				'link_before'     => '',
+				'link_after'      => '',
+				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'           => -1,
+				'walker'          => ''
+			);
+			
+			wp_nav_menu( $principal );
+			
+			$secondaire = array(
+				'theme_location'  => '',
+				'menu'            => 'secondaire',
+				'container'       => '',
+				'container_class' => '',
+				'container_id'    => '',
+				'menu_class'      => 'inline-list',
+				'menu_id'         => 'footer-menu-secondaire',
+				'echo'            => true,
+				'fallback_cb'     => 'wp_page_menu',
+				'before'          => '',
+				'after'           => '',
+				'link_before'     => '',
+				'link_after'      => '',
+				'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'depth'           => 0,
+				'walker'          => ''
+			);
+			
+			wp_nav_menu( $secondaire );
+			
+			?>
 		<?php do_action('foundationPress_after_footer'); ?>
 	</section>
 </footer>
